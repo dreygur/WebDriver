047eb26cbd679056f4ba149a46a7a5be0d8aa285
Rakibul Yeasin <rytotul@gmail.com>
2021-02-12 04:03:41 +0600

    Added OS Support for Kill :fire:

--- webdriver/webdriver.go
+++ webdriver/webdriver.go
@@ -3,7 +3,7 @@
  * Created: Sunday, 7th February 2021 4:30:08 pm
  * Author: Rakibul Yeasin (ryeasin03@gmail.com)
  * -----
- * Last Modified: Friday, 12th February 2021 3:40:31 am
+ * Last Modified: Friday, 12th February 2021 4:01:39 am
  * Modified By: Rakibul Yeasin (ryeasin03@gmail.com)
  * -----
  * Copyright (c) 2021 Slishee
@@ -17,6 +17,8 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"runtime"
+	"strconv"
 
 	"github.com/imroc/req"
 )
@@ -98,4 +100,22 @@ func GetStatus() interface{} {
 	resp.ToJSON(&res)
 	fmt.Println(resp)
 	return resp
+}
+
+// Kill the Server
+func Kill() error {
+	var err error
+	// err := syscall.Kill(srv.pid, syscall.SIGKILL)
+	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+		k := exec.Command("kill", "-9", strconv.Itoa(srv.pid))
+		k.Stderr = os.Stderr
+		k.Stdout = os.Stdout
+		err = k.Run()
+	} else {
+		k := exec.Command("TASKKILL", "/T", "/F", "/PID", strconv.Itoa(srv.pid))
+		k.Stderr = os.Stderr
+		k.Stdout = os.Stdout
+		err = k.Run()
+	}
+	return err
 }
